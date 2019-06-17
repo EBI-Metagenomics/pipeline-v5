4d3480d3d9c27ef8798e50c265f3ca7337d1f260
mscheremetjew <maxim@ebi.ac.uk>
2019-06-17 16:00:25 +0100

    Changed CWL tool install procedure. Deactivated utils and workflow conformance tests.

--- travis/tools-validation-tests.sh
+++ travis/tools-validation-tests.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 export PATH="$HOME/node-v8.11.1:$PATH"
-export PATH="$HOME/miniconda/bin:$PATH"
-source activate cwl-environment
+#export PATH="$HOME/miniconda/bin:$PATH"
+#source activate cwl-environment
 
 set -e
 
-# run a conformance test for all CWL descriptions
+# run a conformance test for all CWL tool descriptions
 
 for i in $(find tools -name "*.cwl"); do
  echo "Testing: ${i}"
--- travis/workflow-cwl-validation-tests.sh
+++ travis/workflow-cwl-validation-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export PATH="$HOME/node-v8.11.1:$PATH"
-export PATH="$HOME/miniconda/bin:$PATH"
-source activate cwl-environment
+#export PATH="$HOME/miniconda/bin:$PATH"
+#source activate cwl-environment
 
 set -e
 
