91ff054505104eed167cfb195c4f62c9389f3d5b
Miguel Boland <migueldboland@gmail.com>
2019-06-28 10:57:51 +0100

    Renamed Diamon -> Diamond

--- travis/run-diamond.blastp-v0.9.21.sh
+++ travis/run-diamond.blastp-v0.9.21.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd tools/Diamond
-CMD="cwl-runner Diamon.blastp-v0.9.21.cwl Diamon.blastp-v0.9.21.test.job.yaml"
+CMD="cwl-runner Diamond.blastp-v0.9.21.cwl Diamond.blastp-v0.9.21.test.job.yaml"
 echo $CMD
 $CMD
