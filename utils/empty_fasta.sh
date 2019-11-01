70ab08e83155001e95b846ee64ee2549b7840aae
Varsha <vkale@ebi.ac.uk>
2019-11-01 14:37:46 +0000

    testing empty fasta fix

--- utils/empty_fasta.sh
+++ utils/empty_fasta.sh
@@ -5,7 +5,7 @@ count=$2
 
 if [ "$count" = 0 ]; then
  echo 'fasta is empty, set dummy'
- echo ">dummy_seq\nATCG" >> "$fasta" | mv "$fasta" 'empty_FASTQ.fasta'
+ echo $'>dummy_seq\nATCG' >> "$fasta" | mv "$fasta" 'empty_FASTQ.fasta'
 else
   echo 'output original files'
   mv "$fasta" "$fasta"
