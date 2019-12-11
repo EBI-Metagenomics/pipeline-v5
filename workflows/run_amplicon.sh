a152bcf1598a1c1672f1ecaff2de5b7776cdcee0
Ekaterina Sakharova <kates@ebi.ac.uk>
2019-12-11 15:35:51 +0000

    changed source run_amplicon.sh

--- workflows/run_amplicon.sh
+++ workflows/run_amplicon.sh
@@ -3,10 +3,7 @@
 # Prepare env
 echo "preparation"
 
-source /hps/nobackup2/production/metagenomics/pipeline/testing/varsha/test_env.rc
-export PATH=$PATH:/homes/emgpr/.nvm/versions/node/v12.10.0/bin/
-export PATH=/hps/nobackup2/production/metagenomics/pipeline/tools-v5/miniconda2-4.6.14/bin:$PATH
-export CONDA_ENV=/hps/nobackup2/production/metagenomics/pipeline/tools-v5/miniconda2-4.6.14/bin/activate
+source /hps/nobackup2/production/metagenomics/pipeline/tools-v5/auto_env.rc
 source /hps/nobackup2/production/metagenomics/pipeline/testing/kate/toil-3.19.0/bin/activate # 3.19 for profiling
 #source /hps/nobackup2/production/metagenomics/pipeline/testing/kate/toil-memory/bin/activate
 #source /hps/nobackup2/production/metagenomics/pipeline/tools-v5/toil-user-env/bin/activate
