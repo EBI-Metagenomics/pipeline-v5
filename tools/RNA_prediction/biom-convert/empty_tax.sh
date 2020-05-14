b3a5507bf61e3a8b8cd56198805d7a2008f02289
Vkale1 <34109092+Vkale1@users.noreply.github.com>
2020-05-14 21:36:31 +0100

    add redirect to new file

--- tools/RNA_prediction/biom-convert/empty_tax.sh
+++ tools/RNA_prediction/biom-convert/empty_tax.sh
@@ -11,12 +11,13 @@ otunotaxid=$6
 y=($( wc -l $mapseq))
 if [ $y -eq 2 ]; then
   echo 'create empty files'
-  mv $mapseq 'empty.mseq'
-  echo -e "1\t1\tsk__NONE" >> $otutable | mv $otutable 'empty.mseq.tsv'
-  mv $biomtable 'empty.txt'
-  mv $krona 'empty.html'
-  mv $fasta "empty.fasta"  # mv $fasta "empty.$db.fasta"
-  echo -e "1\t1\tsk__NONE" >> $otunotaxid | mv $otunotaxid 'empty.notaxid.tsv'
+  touch empty.mseq empty.mseq.tsv empty.txt empty.html empty.fasta empty.notaxid.tsv
+  cat $mapseq  >> empty.mseq
+  cat $biomtable >> empty.txt
+  cat $krona >> empty.html
+  cat $fasta >> empty.fasta  # mv $fasta "empty.$db.fasta"
+  cat $otutable >> empty.mseq.tsv && echo -e '\n1\t1\tsk__NONE' >> empty.mseq.tsv
+  cat $otunotaxid >> empty.notaxid.tsv && echo -e '\n1\t1\tsk__NONE' >> empty.notaxid.tsv
 else
   echo 'output original files'
   mv $mapseq $otutable $biomtable $krona $fasta $otunotaxid .
