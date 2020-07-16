f1bcf1aac3c93b1aed2f82f4be2d77f7620461e3
Ekaterina Sakharova <kates@ebi.ac.uk>
2020-07-16 17:13:16 +0100

    symlinks after rebase

--- docker/scripts_python3/its-length.py
+++ docker/scripts_python3/its-length.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env /hps/nobackup2/production/metagenomics/pipeline/tools-v5/miniconda3-4.6.14/bin/python3
 import glob
 import argparse
 import sys
@@ -9,23 +9,27 @@ import shutil
 
 
 def get_avg_length(masked_its):  # get average length of longest ITS sequences - separated by 'N'
-    if os.path.exists(masked_its):
-        all_lengths = []
-        with gzip.open(masked_its, 'rt') as unzipped_file:
-            for record in SeqIO.parse(unzipped_file, 'fasta'):
-                sequences = [x for x in record.seq.split('N') if x and x != '']
-                longest_seq = {'num': 0, 'letters': ''}
-                for seq in sequences:
-                    length = len(seq)
-                    if length > longest_seq['num']:
-                        longest_seq['num'] = length
-                        longest_seq['letters'] = seq
-                all_lengths.append(longest_seq['num'])
-        return int(sum(all_lengths) / len(all_lengths))
+    if masked_its is not None:
+        if os.path.exists(masked_its):
+            all_lengths = []
+            with gzip.open(masked_its, 'rt') as unzipped_file:
+                for record in SeqIO.parse(unzipped_file, 'fasta'):
+                    sequences = [x for x in record.seq.split('N') if x and x != '']
+                    longest_seq = {'num': 0, 'letters': ''}
+                    for seq in sequences:
+                        length = len(seq)
+                        if length > longest_seq['num']:
+                            longest_seq['num'] = length
+                            longest_seq['letters'] = seq
+                    all_lengths.append(longest_seq['num'])
+            return int(sum(all_lengths) / len(all_lengths))
+        else:
+            return 0
     else:
         return 0
 
-def hits_to_num_ratio(fasta, input_folder):  # ratio of mapseq hits to number of total seqs LSU/SSU
+
+def hits_to_num_ratio(fasta, input_folder):  # ratio of mapseq hits to number of total seqs LSU/SSU                 
     rna_sum, rna_num = [0 for _ in range(2)]
     rna = os.path.join(input_folder, '*.tsv')
     if 'empty' not in os.path.relpath(fasta):
@@ -38,17 +42,16 @@ def hits_to_num_ratio(fasta, input_folder):  # ratio of mapseq hits to number of
     else:
         return 0
 
+
 def validate_hits(ssu_fasta, lsu_fasta, ssu_folder, lsu_folder, len_avg):  # check length and ratio and assign tag
-    ssu_ratio = hits_to_num_ratio(ssu_fasta, ssu_folder)
-    lsu_ratio = hits_to_num_ratio(lsu_fasta, lsu_folder)
+    ssu_ratio = hits_to_num_ratio(ssu_fasta, ssu_folder) if ssu_folder is not None else 0
+    lsu_ratio = hits_to_num_ratio(lsu_fasta, lsu_folder) if lsu_folder is not None else 0
     if len_avg > 200:
         if ssu_ratio or lsu_ratio > 0.1:
             return 'both'
         else:
             return 'ITS'
     elif 120 <= len_avg <= 199:
-        ssu_ratio = hits_to_num_ratio(ssu_fasta, ssu_folder)
-        lsu_ratio = hits_to_num_ratio(lsu_fasta, lsu_folder)
         if ssu_ratio or lsu_ratio > 0.1:
             return 'rRNA'
         else:
@@ -58,39 +61,72 @@ def validate_hits(ssu_fasta, lsu_fasta, ssu_folder, lsu_folder, len_avg):  # che
 
 
 def suppress_dir(flag, lsu, ssu, its, its_file, ssu_file, lsu_file):
-    suppressed_folder, taxonomy_summary, its_filename, lsu_filename, ssu_filename = \
-        [x for x in ['suppressed', 'taxonomy-summary', os.path.basename(its_file), os.path.basename(lsu_file), os.path.basename(ssu_file)]]
+    suppressed_folder = 'suppressed'
     os.mkdir('suppressed')
+    taxonomy_summary = 'taxonomy-summary'
     os.mkdir('taxonomy-summary')
+
+    its_filename = os.path.basename(its_file) if its is not None else ''
+    lsu_filename = os.path.basename(lsu_file) if lsu is not None else ''
+    ssu_filename = os.path.basename(ssu_file) if ssu is not None else ''
+
     # move dir by tag
-    if flag == 'ITS':
-        [shutil.copytree(src, dest) for src, dest in [(lsu, suppressed_folder + '/LSU'), (ssu, suppressed_folder + '/SSU'), (its, taxonomy_summary + '/its')]]
-        [shutil.copy(src, dest) for src, dest in [(lsu_file, suppressed_folder), (ssu_file, suppressed_folder), (its_file, its_filename)]]
-    elif flag == 'rRNA':
-        [shutil.copytree(src, dest) for src, dest in [(lsu, taxonomy_summary + '/LSU'), (ssu, taxonomy_summary + '/SSU'), (its, suppressed_folder + '/its')]]
-        [shutil.copy(src, dest) for src, dest in [(lsu_file, lsu_filename), (ssu_file, ssu_filename), (its_file, suppressed_folder)]]
-    elif flag == 'both':
-        [shutil.copytree(src, taxonomy_summary + dest) for src, dest in [(lsu, '/LSU'), (ssu, '/SSU'), (its, '/its')]]
-        [shutil.copy(src, dest) for src, dest in [(lsu_file, lsu_filename), (ssu_file, ssu_filename), (its_file, its_filename)]]
+    list_folders, list_files = [[] for _ in range(2)]
+    addition = ''
+    for folder, name, cur_file, filename in zip([lsu, ssu, its],
+                                                ['/LSU', '/SSU', '/its'],
+                                                [lsu_file, ssu_file, its_file],
+                                                [lsu_filename, ssu_filename, its_filename]):
+        if folder is not None:
+            if flag == 'ITS':
+                if name == '/its':
+                    list_folders.append((folder, taxonomy_summary + name))
+                    list_files.append((cur_file, filename))
+                else:
+                    list_folders.append((folder, suppressed_folder + name))
+                    list_files.append((cur_file, suppressed_folder))
+            elif flag == 'rRNA':
+                if name == '/its':
+                    list_folders.append((folder, suppressed_folder + name))
+                    list_files.append((cur_file, suppressed_folder))
+                else:
+                    list_folders.append((folder, taxonomy_summary + name))
+                    list_files.append((cur_file, filename))
+            elif flag == 'both':
+                list_folders.append((folder, name))
+                list_files.append((cur_file, filename))
+                addition = taxonomy_summary
+
+    [shutil.copytree(src, addition + dest) for src, dest in list_folders]
+    [shutil.copy(src, dest) for src, dest in list_files]
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="get average length of ITS sequences and suppress unwanted folders")
-    parser.add_argument("--lsu-file", dest="lsu_file", help="lsu fasta")
-    parser.add_argument("--ssu-file", dest="ssu_file", help="ssu fasta")
-    parser.add_argument("--its-file", dest="its_file", help="its fasta")
-    parser.add_argument("--lsu-dir", dest="lsu_directory", help="directory in path taxonomy-summary/LSU")
-    parser.add_argument("--ssu-dir", dest="ssu_directory", help="directory in path taxonomy-summary/SSU")
-    parser.add_argument("--its-dir", dest="its_directory", help="directory in path taxonomy-summary/its")
+    parser.add_argument("--lsu-file", dest="lsu_file", help="lsu fasta", required=False, default=None)
+    parser.add_argument("--ssu-file", dest="ssu_file", help="ssu fasta", required=False, default=None)
+    parser.add_argument("--its-file", dest="its_file", help="its fasta", required=False, default=None)
+    parser.add_argument("--lsu-dir", dest="lsu_directory", help="directory in path taxonomy-summary/LSU",
+                        required=False, default=None)
+    parser.add_argument("--ssu-dir", dest="ssu_directory", help="directory in path taxonomy-summary/SSU",
+                        required=False, default=None)
+    parser.add_argument("--its-dir", dest="its_directory", help="directory in path taxonomy-summary/its",
+                        required=False, default=None)
 
 
-    if len(sys.argv) < 6:
+    if len(sys.argv) < 3:
         parser.print_help()
     else:
         args = parser.parse_args()
         avg = get_avg_length(args.its_file)
         print('average ITS length is ' + str(avg))
-        validate_hits(args.ssu_file, args.lsu_file, args.ssu_directory, args.lsu_directory, avg)
         print('suppressing...')
         suppress_flag = validate_hits(args.ssu_file, args.lsu_file, args.ssu_directory, args.lsu_directory, avg)
-        suppress_dir(suppress_flag, args.lsu_directory, args.ssu_directory, args.its_directory, args.its_file, args.ssu_file, args.lsu_file)
+        print(suppress_flag)
+        suppress_dir(suppress_flag, args.lsu_directory, args.ssu_directory, args.its_directory, args.its_file,
+                     args.ssu_file, args.lsu_file)
+        if len(os.listdir('suppressed')) == 0:
+            os.rmdir('suppressed')
+        if len(os.listdir('taxonomy-summary')) == 0:
+            os.rmdir('taxonomy-summary')
+
