b00f360e9f005d83102b461017284b7adc1785f5
Ekaterina Sakharova <kates@ebi.ac.uk>
2020-11-13 09:49:00 +0000

    change count_lines.py script

--- docker/scripts_python3/count_lines.py
+++ docker/scripts_python3/count_lines.py
@@ -3,6 +3,8 @@
 
 import sys
 import argparse
+import os
+import gzip
 
 
 if __name__ == "__main__":
@@ -15,7 +17,12 @@ if __name__ == "__main__":
     else:
         args = parser.parse_args()
         count = 0
-        with open(args.input, 'r') as input_file, open('data.txt', 'w') as output_file:
+        with open('data.txt', 'w') as output_file:
+            if '.gz' in os.path.basename(args.input):
+                input_file = gzip.open(args.input, 'rb')
+            else:
+                input_file = open(args.input, 'r')
             for line in input_file:
                 count += 1
             output_file.write(str(int(count/int(args.number))))
+            input_file.close()
