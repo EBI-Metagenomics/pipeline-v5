19c505f5166375c4bed270b5be9206da88401204
Martin Beracochea <mbc@ebi.ac.uk>
2021-11-17 19:18:44 +0000

    Fix count_lines.py python path

--- utils/count_lines/count_lines.py
+++ utils/count_lines/count_lines.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env /hps/nobackup2/production/metagenomics/pipeline/tools-v5/miniconda3-4.6.14/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import sys
@@ -9,20 +9,20 @@ import gzip
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Count lines in fastq")
-    parser.add_argument("-f", "--input", dest="input", help="Input fastq", required=True)
-    parser.add_argument("-n", "--number", dest="number", help="number to divide", required=True)
-
-    if len(sys.argv) == 1:
-        parser.print_help()
-    else:
-        args = parser.parse_args()
-        count = 0
-        with open('data.txt', 'w') as output_file:
-            if '.gz' in os.path.basename(args.input):
-                input_file = gzip.open(args.input, 'rb')
-            else:
-                input_file = open(args.input, 'r')
-            for line in input_file:
-                count += 1
-            output_file.write(str(int(count/int(args.number))))
-            input_file.close()
+    parser.add_argument(
+        "-f", "--input", dest="input", help="Input fastq", required=True
+    )
+    parser.add_argument(
+        "-n", "--number", dest="number", help="number to divide", required=True
+    )
+    args = parser.parse_args()
+    count = 0
+    with open("data.txt", "w") as output_file:
+        if ".gz" in os.path.basename(args.input):
+            input_file = gzip.open(args.input, "rb")
+        else:
+            input_file = open(args.input, "r")
+        for line in input_file:
+            count += 1
+        output_file.write(str(int(count / int(args.number))))
+        input_file.close()
