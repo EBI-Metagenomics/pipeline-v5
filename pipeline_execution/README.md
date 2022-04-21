6377d346a09a6f0665ff2db70b0d0384825ad7ac
Varsha <vkale@ebi.ac.uk>
2022-04-21 11:50:27 +0100

    pipeline execution scripts for rna prediction using slurm

---
+++ pipeline_execution/README.md
@@ -0,0 +1,65 @@
+[![Build Status](https://travis-ci.org/EBI-Metagenomics/pipeline-v5.svg?branch=master)](https://travis-ci.com/EBI-Metagenomics/pipeline-v5)
+
+# Pipeline execution 
+
+This repository contains scripts for execution of CWL workflows
+
+## QC and RNA prediction workflow
+
+This workflow runs a conditional QC step, RNA prediction and classification against SILVA databases.
+
+####Required databases 
+
+```bash
+mkdir ref-dbs && cd ref-dbs && 
+# download silva dbs
+mkdir silva_ssu silva_lsu
+wget \
+  ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/pipeline-5.0/ref-dbs/silva_ssu-20200130.tar.gz \
+  ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/pipeline-5.0/ref-dbs/silva_lsu-20200130.tar.gz 
+tar --extract --gzip --directory=silva_ssu silva_ssu-20200130.tar.gz
+tar --extract --gzip --directory=silva_lsu silva_lsu-20200130.tar.gz
+mkdir ribosomal
+wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/pipeline-5.0/ref-dbs/rfam_models/ribosomal_models/RF*.cm \
+  ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/pipeline-5.0/ref-dbs/rfam_models/ribosomal_models/ribo.claninfo \
+  -P ribosomal 
+
+# rRNA.claninfo
+wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/pipeline-5.0/ref-dbs/rRNA.claninfo
+# other Rfam models
+mkdir other
+wget ftp://ftp.ebi.ac.uk/pub/databases/metagenomics/pipeline-5.0/ref-dbs/rfam_models/other_models/*.cm \
+  -P other 
+```
+
+####Execution
+
+Source the tool and CWL environments before executing the following script.
+
+The [rna_prediction_slurm.sh](scripts/rna_prediction_slurm.sh) script is used to trigger the CWL + Toil workflows using the SLURM batch system.
+
+```shell
+$ rna_prediction_slurm.sh -h
+
+Run MGnify pipeline.
+Script arguments.
+  Resources:
+  -m                  Memory to use to with toil --defaultMemory. (optional, default 50G)
+  -n                  Number of cpus to use with toil --defaultCores. (optional, default 4)
+  -l                  Limit number of jobs to schedule. (optional, default 100)
+
+  Pipeline parameters:
+  -y                  template yml file. (optional, default ../templates/rna_prediction_template.yml)
+  -f                  Forward reads fasta file path.
+  -r                  Reverse reads fasta file path.
+  -s                  Single reads fasta file path.
+  -q                  Run qc ('true' or 'false').
+  -n                  Name of run and prefix to output files.
+  -d                  Path to run directory.
+  -p                  Path to database directory. (optional, default ../ref-dbs)
+```
+
+
+
+
+
---
+++ pipeline_execution/scripts/rna_prediction_slurm.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+#default values
+MEMORY=50G
+NUM_CORES=4
+LIMIT_QUEUE=100
+YML='../templates/rna_prediction_template.yml'
+DB_DIR='../ref-dbs'
+
+_usage() {
+    echo "
+Run MGnify pipeline.
+Script arguments.
+  Resources:
+  -m                  Memory to use to with toil --defaultMemory. (optional, default ${MEMORY})
+  -n                  Number of cpus to use with toil --defaultCores. (optional, default ${NUM_CORES})
+  -l                  Limit number of jobs to schedule. (optional, default ${LIMIT_QUEUE})
+
+  Pipeline parameters:
+  -y                  template yml file. (optional, default ${YML})
+  -f                  Forward reads fasta file path.
+  -r                  Reverse reads fasta file path.
+  -s                  Single reads fasta file path.
+  -q                  Run qc ('true' or 'false').
+  -n                  Name of run and prefix to output files.
+  -d                  Path to run directory.
+  -p                  Path to database directory. (optional, default ${DB_DIR})
+"
+}
+
+while getopts :y:f:r:s:q:n:d:m:n:l:p:h option; do
+    case "${option}" in
+    y) YML=${OPTARG} ;;
+    f)
+        echo "presented paired-end forward path"
+        FORWARD_READS=${OPTARG}
+        ;;
+    r)
+        echo "presented paired-end reverse path"
+        REVERSE_READS=${OPTARG}
+        ;;
+    s)
+        echo "presented single-end path"
+        SINGLE=${OPTARG}
+        ;;
+    q) QC=${OPTARG} ;;
+    n) NAME=${OPTARG} ;;
+    d) RUN_DIR=${OPTARG} ;;
+    m) MEMORY=${OPTARG} ;;
+    n) NUM_CORES=${OPTARG} ;;
+    l) LIMIT_QUEUE=${OPTARG} ;;
+    p) DB_DIR=${OPTARG} ;;
+    h)
+        _usage
+        exit 0
+        ;;
+    esac
+done
+
+# ----------------------------- sanity check arguments ----------------------------- #
+
+_check_mandatory() {
+    # Check if the argument is empty or null
+    # $1 variable
+    # $2 name to show
+    if [ -z "$1" ]; then
+        echo "Error." >&2
+        echo "Option ${2} is mandatory " >&2
+        echo "type -h to get help"
+        exit 1
+    fi
+}
+
+
+_check_reads() {
+  #check forward and reverse reads both present
+  #check if single reads then no other readsgiven
+  if [ -z "$3" ]; then
+   if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Error"
+    echo "Only provide one format: paired or single reads"
+    exit 1
+    fi
+  fi
+
+  if [ -z "$1" ] && [ -n "$2" ]; then
+    echo "Error"
+    echo "only reverse reads given, provide forward with -f"
+    exit 1
+  fi
+
+  if [ -n "$1" ] && [ -z "$2" ]; then
+    echo "Error"
+    echo "only forward reads given, provide reverse with -r"
+    exit 1
+  fi
+
+}
+
+_check_mandatory "$QC" "-q"
+_check_mandatory "$NAME" "-n"
+_check_mandatory "$RUN_DIR" "-d"
+_check_reads "$FORWARD_READS" "$REVERSE_READS" "$SINGLE"
+
+#get format from input
+if [ -n "$SINGLE" ]; then
+  TYPE="single"
+else
+  TYPE="paired"
+fi
+
+# ----------------------------- environment & variables ----------------------------- #
+
+# load required environments and packages before running
+
+export TOIL_SLURM_ARGS="--array=1-${LIMIT_QUEUE}%20" #schedule 100 jobs 20 running at one time
+
+export CWL=../../workflows/subworkflows/qc-merge-rna.cwl
+
+# work dir
+export WORK_DIR=${RUN_DIR}/work-dir
+export JOB_TOIL_FOLDER=${WORK_DIR}/job-store-wf
+export TMPDIR=${RUN_DIR}/tmp
+
+# result dir
+export OUT_DIR=${RUN_DIR}
+export LOG_DIR=${OUT_DIR}/log-dir/${NAME}
+export OUT_DIR_FINAL=${OUT_DIR}/results/${NAME}
+
+mkdir -p "${LOG_DIR}" "${OUT_DIR_FINAL}" "${JOB_TOIL_FOLDER}" "${TMPDIR}"
+
+export RENAMED_YML=${RUN_DIR}/"${NAME}".yml
+
+
+# ----------------------------- prepare yml file ----------------------------- #
+
+echo "Writing yaml file"
+python3 "prepare_yml.py" \
+    -y "${YML}" \
+    -o "${RENAMED_YML}" \
+    -a "raw-reads" \
+    -t "${TYPE}" \
+    -s "${SINGLE}" \
+    -f "${FORWARD_READS}" \
+    -r "${REVERSE_READS}" \
+    -d "${DB_DIR}"
+
+echo "run_qc: ${QC}" >> "${RENAMED_YML}"
+
+
+# ----------------------------- running pipeline ----------------------------- #
+
+CMD="toil-cwl-runner \
+  --preserve-entire-environment \
+  --enable-dev \
+  --logFile "${LOG_DIR}"/"${NAME}".log \
+  --jobStore "${JOB_TOIL_FOLDER}"/"${NAME}" \
+  --outdir "${OUT_DIR_FINAL}" \
+  --batchSystem SLURM \
+  --disableChaining \
+  --disableProgress \
+  --disableCaching \
+  --defaultMemory "${MEMORY}" \
+  --defaultCores "${NUM_CORES}" \
+  --retryCount 0 \
+  --no-container \
+  $CWL \
+  $RENAMED_YML"
+
+echo "$CMD"
+$CMD
+
